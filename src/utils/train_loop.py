c0b119d99cc787f26e6a1eb044ca5852250a25f2
DimensionSTP <ddang8jh@gmail.com>
2024-10-08 15:18:20 +0900

    feat: save as hf form at master pod

--- src/utils/train_loop.py
+++ src/utils/train_loop.py
@@ -1,4 +1,4 @@
-import math
+import numpy as np
 
 from omegaconf import DictConfig
 
@@ -6,14 +6,17 @@ import torch
 from torch.utils.data import DataLoader
 from torch import optim
 
+from torch_xla import runtime as xr
 from torch_xla.core import xla_model as xm
 from torch_xla.distributed import parallel_loader
+from torch_xla.experimental.xla_sharding import Mesh
 from torch_xla.experimental import xla_sharding as xs
-from torch_xla.distributed.fsdp.utils import apply_xla_patch_to_nn_linear
 
 from transformers import AutoModelForCausalLM
 
 import wandb
+
+from .spmd_setup import partition_module
 
 
 def train_loop(
@@ -29,70 +32,85 @@ def train_loop(
         name=f"{config.model_type}-max_length={config.max_length}-lr={config.lr}",
     )
 
+    xr.use_spmd()
+
     device = xm.xla_device()
     print(f"Using XLA device: {device}")
 
-    train_loader = parallel_loader.MpDeviceLoader(
+    para_train_loader = parallel_loader.MpDeviceLoader(
         train_loader,
         device,
     )
-    val_loader = parallel_loader.MpDeviceLoader(
+    para_val_loader = parallel_loader.MpDeviceLoader(
         val_loader,
         device,
     )
 
-    print("Checking model parameters before wrapping with FSDP:")
-    for name, param in model.named_parameters():
-        if param.numel() == 0:
-            print(f"Warning: Parameter {name} has size 0!")
-        print(f"Parameter {name} - shape: {param.shape}")
+    num_devices = xr.global_runtime_device_count()
+    mesh_shape = (
+        1,
+        num_devices,
+        1,
+        1,
+    )
+    device_ids = np.array(range(num_devices))
+    mesh = Mesh(
+        device_ids,
+        mesh_shape,
+        (
+            "dp",
+            "fsdp",
+            "mp",
+            "sp",
+        ),
+    )
 
-    xm.rendezvous("before_fsdp")
+    xm.rendezvous("before_smpd")
     try:
-        fsdp_model = apply_xla_patch_to_nn_linear(
+        partition_module(
             model,
-            xs.xla_patched_nn_linear_forward,
-        ).to(device)
-        print("Model successfully wrapped with FSDP.")
+            mesh,
+            verbose=True,
+        )
+        print("Model successfully wrapped with SMPD.")
     except Exception as e:
-        print(f"Error in FSDP wrapping: {e}")
+        xm.master_print(f"Error in SMPD wrapping: {e}")
         return
 
     gradient_accumulation_steps = config.gradient_accumulation_steps
 
-    num_train_samples = len(train_loader._loader.dataset)
-    effective_batch_size = (
-        config.per_device_train_batch_size
-        * config.gradient_accumulation_steps
-        * config.tpu_num_cores
-    )
-    total_steps = (
-        math.ceil(num_train_samples / effective_batch_size) * config.num_train_epochs
-    )
-    print(f"Total steps: {total_steps}, Effective batch size: {effective_batch_size}")
-
     global_step = 0
     accumulation_loss = 0.0
     for epoch in range(config.num_train_epochs):
-        fsdp_model.train()
-        print(f"Starting epoch {epoch + 1}/{config.num_train_epochs}")
+        model.train()
+        xm.master_print(f"Starting epoch {epoch + 1}/{config.num_train_epochs}")
 
-        for step, inputs in enumerate(train_loader):
-            if "input_ids" not in inputs or inputs["input_ids"].size(0) == 0:
+        for step, batch in enumerate(para_train_loader):
+            if "input_ids" not in batch or batch["input_ids"].size(0) == 0:
                 if step == 0:
-                    print(f"Warning: Empty batch or missing 'input_ids'")
+                    xm.master_print(f"Warning: Empty batch or missing 'input_ids'")
                 continue
 
             try:
-                inputs = {k: v.to(device) for k, v in inputs.items()}
-                outputs = fsdp_model(**inputs)
+                input_ids = batch["input_ids"].to(device)
+                attention_mask = batch["attention_mask"].to(device)
+                labels = batch["labels"].to(device)
+                xs.mark_sharding(input_ids, mesh, (0, 1))
+                xs.mark_sharding(attention_mask, mesh, (0, 1))
+                xs.mark_sharding(labels, mesh, (0, 1))
+                outputs = model(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    labels=labels,
+                )
+
                 loss = outputs.loss / gradient_accumulation_steps
                 loss.backward()
 
                 accumulation_loss += loss.item()
             except Exception as e:
                 if step == 0:
-                    print(f"Error during forward/backward pass")
+                    xm.master_print(f"Error during forward/backward pass")
                 continue
 
             if (step + 1) % gradient_accumulation_steps == 0 or (step + 1) == len(
@@ -100,7 +118,7 @@ def train_loop(
             ):
                 try:
                     torch.nn.utils.clip_grad_norm_(
-                        fsdp_model.parameters(),
+                        model.parameters(),
                         config.max_grad_norm,
                     )
 
@@ -116,50 +134,74 @@ def train_loop(
                                 "global_step": global_step,
                             }
                         )
-                        print(
+                        xm.master_print(
                             f"Global step {global_step}, Accumulated Loss: {accumulation_loss}"
                         )
                         accumulation_loss = 0.0
 
                     if global_step % config.save_steps == 0:
-                        xm.rendezvous("saving_checkpoint")
-                        xm.save(
-                            fsdp_model.state_dict(),
-                            f"{config.put_dif}/checkpoint-{global_step}.pt",
+                        xm.rendezvous(f"saving checkpoint-{global_step}")
+                        model_state = model.state_dict()
+                        xm.all_reduce(
+                            "sum",
+                            model_state,
                         )
-                        print(
-                            f"checkpoint-{global_step}.pt saved at {config.output_dir}"
+                        if xm.is_master_ordinal():
+                            cpu_model = model.cpu()
+                            cpu_model.load_state_dict(model_state)
+                            cpu_model.save_pretrained(
+                                f"{config.output_dir}/checkpoint-{global_step}"
+                            )
+                        xm.master_print(
+                            f"checkpoint-{global_step} saved at {config.output_dir}"
                         )
 
                 except Exception as e:
-                    print(f"Error during optimizer step at step {global_step}: {e}")
+                    xm.master_print(
+                        f"Error during optimizer step at step {global_step}: {e}"
+                    )
                     continue
 
-        fsdp_model.eval()
+        model.eval()
         val_loss = 0.0
-        for inputs in val_loader:
+        for inputs in para_val_loader:
             if "input_ids" not in inputs or inputs["input_ids"].size(0) == 0:
-                print("Warning: Empty batch or missing 'input_ids' during validation")
+                xm.master_print(
+                    "Warning: Empty batch or missing 'input_ids' during validation"
+                )
                 continue
 
             try:
                 inputs = {k: v.to(device) for k, v in inputs.items()}
                 with torch.no_grad():
-                    outputs = fsdp_model(**inputs)
+                    outputs = model(**inputs)
                     val_loss += outputs.loss.item()
             except Exception as e:
-                print(f"Error during validation step: {e}")
+                xm.master_print(f"Error during validation step: {e}")
                 continue
 
         avg_val_loss = val_loss / len(val_loader)
-        print(f"Epoch {epoch + 1} Validation Loss: {avg_val_loss}")
-        wandb.log({"val_loss": avg_val_loss, "epoch": epoch + 1})
+        xm.master_print(f"Epoch {epoch + 1} Validation Loss: {avg_val_loss}")
+        wandb.log(
+            {
+                "val_loss": avg_val_loss,
+                "epoch": epoch + 1,
+            }
+        )
 
     try:
-        xm.rendezvous("saving_model")
-        xm.save(fsdp_model.state_dict(), f"{config.output_dir}/final_model.pt")
-        print(f"final_model.pt saved at {config.output_dir}")
+        xm.rendezvous(f"saving last checkpoint")
+        model_state = model.state_dict()
+        xm.all_reduce(
+            "sum",
+            model_state,
+        )
+        if xm.is_master_ordinal():
+            cpu_model = model.cpu()
+            cpu_model.load_state_dict(model_state)
+            cpu_model.save_pretrained(f"{config.output_dir}/last")
+        xm.master_print(f"last checkpoin saved at {config.output_dir}")
     except Exception as e:
-        print(f"Error saving final model: {e}")
+        xm.master_print(f"Error saving final model: {e}")
 
     wandb.finish()
